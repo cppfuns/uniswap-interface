5be8dac2a56bb20dc7540c608e69c99c4ab62be5
Surou <652511569@qq.com>
2021-03-22 19:40:52 +0800

    fix# code

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -3,7 +3,7 @@ import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
-  [ChainId.TESTNET]: '0x77dCa2C955b15e9dE4dbBCf1246B4B85b651e50e'
+  [ChainId.TESTNET]: '0x2A16f8A9C88Ddaad5928D85Ce065940b7b25E11c'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
