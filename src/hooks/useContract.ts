ceb0ef46a09392f408a718f40fbf7f3864a65dc8
Surou <652511569@qq.com>
2021-03-22 18:05:10 +0800

    fix# code

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -75,9 +75,7 @@ export function useENSRegistrarContract(withSignerIfPossible?: boolean): Contrac
   if (chainId) {
     switch (chainId) {
       case ChainId.MAINNET:
-      case ChainId.GÖRLI:
-      case ChainId.ROPSTEN:
-      case ChainId.RINKEBY:
+      case ChainId.TESTNET:
         address = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
         break
     }
